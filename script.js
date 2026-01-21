f9cc97154d618deacd7832ef60bc7914abde54c6
Alishakattel <kattelalisha11@gmail.com>
2026-01-21 22:41:20 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+let temp=15;
+if(temp<10){
+    console.log("Wear a heavy jacket.");
+}
+else if(temp>=10 && temp<=20){
+    console.log("A light sweater will do.")
+}
+else if(temp>=21 && temp<=30){
+    console.log("Enjoy the T-shirt weather.")
+}
+else{
+    console.log("It's hot! Stay hydrated.")
+}
+
+
+let score=80;
+if(score >=90){
+    console.log("A");
+}
+else if(score >=80){
+    console.log("B");
+}
+else if(score >=70){
+    console.log("C");
+}
+else{
+    console.log("F");
+}
